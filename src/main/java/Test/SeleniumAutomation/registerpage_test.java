e504564b2cff59cc4d7770c306e273421c4da857
SayaliKadam21 <sayalikadam600@gmail.com>
2026-05-12 21:24:10 +0530

    Register page test

---
+++ src/main/java/Test/SeleniumAutomation/registerpage_test.java
@@ -0,0 +1,11 @@
+package Test.SeleniumAutomation;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class registerpage_test {
+	public static void main(String[]args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://demo.automationtesting.in/Register.html");
+	}
+
+}
